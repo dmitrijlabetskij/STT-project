3bb9c7dbd15eae5d02832f546ffa8249559bc7b9
dmitrijlabetskij <dima.labetcky@gmail.com>
2025-01-16 23:55:17 +0500

    Добавлено распознавание голосовых

--- bot/mybot.py
+++ bot/mybot.py
@@ -6,6 +6,7 @@ import os
 from main import Diarization
 from collections import deque
 import uuid
+import subprocess
 
 
 class TelegramBot:
@@ -21,7 +22,7 @@ class TelegramBot:
 
     async def start(self, update: Update, context: CallbackContext) -> None:
         user = update.effective_user
-        message = f"Привет, {user.first_name}! Отправь голосовое сообщение"
+        message = f"Привет, {user.first_name}! Отправь голосовое или mp3."
         keyboard = [[InlineKeyboardButton("Info", callback_data="button_click")]]
         reply_markup = InlineKeyboardMarkup(keyboard)
         await update.message.reply_text(message, reply_markup=reply_markup)
@@ -29,7 +30,7 @@ class TelegramBot:
     async def button(self, update: Update, context: CallbackContext) -> None:
         instructions = [
             "Инструкция по использованию бота:",
-            "1. Отправьте голосовое сообщение или аудиофайл.",
+            "1. Отправьте аудиофайл или голосовое сообщение.",
             "2. Дождитесь завершения обработки.",
             "3. Получите результат в формате текста."
         ]
@@ -54,11 +55,40 @@ class TelegramBot:
 
         # Запускаем обработку в фоновом потоке
         asyncio.create_task(self.process_audio(update, context, user_id, file_name))  # Передаем все аргументы
+        
+    async def handle_voice_message(self, update: Update, context: CallbackContext) -> None:
+        user_id = update.message.from_user.id
+        
+        # Добавляем пользователя в очередь и сразу выводим его позицию
+        self.queue.append(user_id)
+
+        # Отправляем пользователю его место в очереди
+        pos = len(self.queue)
+        await update.message.reply_text(f"Подождите, идёт обработка...\nВаше место в очереди на обработку: {pos}")
+
+        # После этого запускаем фоновую задачу для обработки аудио
+        user = update.effective_user
+        unique_id = str(uuid.uuid4())  # Генерируем уникальный ID
+        file_name = f"{user.id}_{unique_id}.ogg"
+
+        # Запускаем обработку в фоновом потоке
+        asyncio.create_task(self.process_voice(update, context, user_id, file_name))  # Передаем все аргументы
+        
+    def convert_to_mp3(self, input, output):
+        """Конвертирует OGG (Opus) в MP3 с помощью ffmpeg."""
+        command = ["ffmpeg", "-i", input, "-acodec", "libmp3lame", output]
+        subprocess.run(command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     async def process_audio(self, update: Update, context: CallbackContext, user_id: int, file_name: str):
         try:
             # Загрузка файла
-            file = await context.bot.get_file(update.message.audio.file_id)
+            if update.message.audio:
+                file_id = update.message.audio.file_id
+            else:
+                await update.message.reply_text("Ошибка: отправьте голосовое или аудиофайл.")
+                return
+            file_id = update.message.audio.file_id or update.message.voice.file_id
+            file = await context.bot.get_file(file_id)
             await file.download_to_drive(custom_path=file_name)
 
             if not os.path.exists(file_name):
@@ -68,6 +98,32 @@ class TelegramBot:
 
             # Запуск обработки в отдельном потоке
             await self.process_diarization_for_user(update, context, user_id, file_name)
+
+        except Exception as e:
+            self.logger.error(f"Ошибка обработки файла: {e}")
+            await update.message.reply_text(f"Произошла ошибка: {e}")
+            
+    async def process_voice(self, update: Update, context: CallbackContext, user_id: int, file_name: str):
+        try:
+            # Загрузка файла
+            if update.message.voice:
+                file_id = update.message.voice.file_id
+            else:
+                await update.message.reply_text("Ошибка: отправьте голосовое или аудиофайл.")
+                return
+            file = await context.bot.get_file(file_id)
+            await file.download_to_drive(custom_path=file_name)
+
+            if not os.path.exists(file_name):
+                raise FileNotFoundError(f"Файл {file_name} не найден после загрузки.")
+
+            self.logger.info(f"Файл {file_name} успешно загружен.")
+
+            # Запуск обработки в отдельном потоке
+            new_file_name = file_name.split('.')[0] + '.mp3'
+            self.convert_to_mp3(file_name, new_file_name)
+            os.remove(file_name)
+            await self.process_diarization_for_user(update, context, user_id, new_file_name)
 
         except Exception as e:
             self.logger.error(f"Ошибка обработки файла: {e}")
@@ -122,6 +178,19 @@ class TelegramBot:
 
     async def handle_other_messages(self, update: Update, context: CallbackContext) -> None:
         await update.message.reply_text("Отправьте аудио для распознавания.")
+        
+    async def handle_voice(self, update: Update, context: CallbackContext):
+        try:
+            '''
+            user = update.effective_user
+            file_name = f"{user.id}.ogg"
+            # Загрузка файла
+            file_id = update.message.audio.file_id or update.message.voice.file_id
+            file = await context.bot.get_file(file_id)
+            await file.download_to_drive(custom_path=file_name)'''
+            logging.info(update.message)
+        except Exception as e:
+            self.logger.error(f"Ошибка обработки файла: {e}")
 
     async def get_user_by_id(self, user_id: int):
         # Получаем пользователя по ID, это пример. Нужно будет связать его с соответствующими данными.
@@ -133,6 +202,7 @@ class TelegramBot:
         application.add_handler(CommandHandler("start", self.start))
         application.add_handler(CallbackQueryHandler(self.button, pattern="button_click"))
         application.add_handler(MessageHandler(filters.AUDIO, self.handle_audio_message))
+        application.add_handler(MessageHandler(filters.VOICE, self.handle_voice_message))
         application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_other_messages))
         application.run_polling(allowed_updates=Update.ALL_TYPES)
 
